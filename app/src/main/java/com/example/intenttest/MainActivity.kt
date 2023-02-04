eca299b1b5ee6250f818e945dcc2ee6938c385a6
NatpacanSribanhad <natpacan.s@kkumail.com>
2023-02-04 20:54:21 +0700

    first commit

--- app/src/main/java/com/example/intenttest/MainActivity.kt
+++ app/src/main/java/com/example/intenttest/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.RadioButton
-import android.widget.Toast
 import com.example.intenttest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
